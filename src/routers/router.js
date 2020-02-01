8a1911c4fc95f2e304c7189e46e48c072f32f186
ank-21 <ankitSrivastava21598@gmail.com>
2020-02-01 09:43:59 +0530

    src file added

---
+++ src/routers/router.js
@@ -0,0 +1,27 @@
+const express = require('express');
+// const passport = require('passport');
+const router = express.Router();
+const NGO = require('../models/ngo');
+// const initializePassport = require('./passport-config')
+
+// initializePassport(
+//   passport,
+//   email => users.find(user => user.email === email),
+//   id => users.find(user => user.id === id)
+// )
+router.post('/signup', async(req,res)=> {
+    console.log(req.body);
+    const ngo = new NGO(req.body);
+    try {
+            await ngo.save();
+            console.log(ngo);
+            res.render("portfolio",{
+              ngo
+            });
+    } catch (error) {
+        res.render("error");
+    }
+})
+
+
+module.exports = router
