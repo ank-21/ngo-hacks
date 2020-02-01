2c92d4f2eaee9a876bf92869cbaf3257831a15a9
ank-21 <ankitSrivastava21598@gmail.com>
2020-02-01 19:35:34 +0530

    password changed of gmail

--- src/auth/keys.js
+++ src/auth/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     auth: {
       email: 'ankitsrivastava21345@gmail.com',
-      pass: 'ankit21@nits',
+      pass: 'ankit21@nits12',
       accountSid: 'ACb47c951abffb33be31e4fe974869e000',
       authToken: '5cfdc5bcb7035b941c066dfeaa006ddc',
       govtemail: 'ankitsrivastava21598@gmail.com',
